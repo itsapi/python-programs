55044e11999d32f9d624994193d4959753653c0c
olls <olls@ovi.com>
2013-10-28 14:29:52 +0000

    Merged networkGame.py

--- networkGame.py
+++ networkGame.py
@@ -13,9 +13,13 @@ network.putData([['x',0]])
 try:
     while True:
         data = network.getData()
+        
         val = input(':')
-        val = int(val) + network.getKey('x')
-        network.putKey('x', val)
+        try:
+            val = int(val) + network.getKey('x')
+            network.putKey('x', val)
+        except ValueError:
+            print('Invalid input.')
 
         print(network.client.get('x'))
 
