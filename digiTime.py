832e3dddafc74e23bb824cbcc1df0ae96ff054cd
Geraint White <geraintwhite@gmail.com>
2013-10-29 17:38:34 +0000

    Added smallest screen recognition

---
+++ digiTime.py
@@ -0,0 +1,31 @@
+import time
+import sys
+import console
+import letters
+import os
+
+while True:
+	text = ''
+
+	for char in os.popen('date').read():
+                if char != '\n' and char != '\t':
+                        text += char
+
+	text = letters.word(text)
+
+	lines = []
+	line = ''
+	for char in text:
+		line += char
+		if char == '\n':
+			lines.append((' '*console.WIDTH) + line[:len(line)-1])
+			line = ''
+
+	i = 0
+	for i in range(len(lines[0])):
+		print('\n'*int(console.HEIGHT/2-2))
+		for line in lines:
+			print((line[i % len(line):] + line[:i % len(line)])[:console.WIDTH])
+		print('\n'*int(console.HEIGHT/2-3))
+		i += 1
+		time.sleep(.02)
--- muliplayer_test.py
+++ muliplayer_test.py
@@ -7,14 +7,23 @@ def main(stdscr, network):
     curses.curs_set(0)
     stdscr.nodelay(1)
 
-    network.putKey('x',1)
-    network.putKey('y',1)
+    maxy, maxx = stdscr.getmaxyx()
+    network.putKey('maxy', maxy)
+    network.putKey('maxx', maxx)
+
+    othery = int(network.client.get('maxy'))
+    otherx = int(network.client.get('maxx'))
+
+    maxy = min((maxy, othery))
+    maxx = min((maxx, otherx))
+
+    network.putKey('x', 1)
+    network.putKey('y', 1)
 
     py, px = 1, 1
 
     try:
         while True:
-            maxy, maxx = stdscr.getmaxyx()
             x, y = int(network.getKey('x')), int(network.getKey('y'))
             xold, yold = x, y
             pxold, pyold = px, py
